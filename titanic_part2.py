bc9c627ac17dfb0316f05d702c3837955334ca03
David Campelo <david.campelo@gmail.com>
2017-07-19 22:37:32 +0100

    Creating the first decision tree - Build the target and features_one numpy arrays. The target will be based on the Survived column in train. The features array will be based on the variables Passenger, Class, Sex, Age, and Passenger Fare - Build a decision tree my_tree_one to predict survival using features_one and target - Look at the importance of features in your tree and compute the score

--- titanic_part2.py
+++ titanic_part2.py
@@ -1,5 +1,7 @@
 # Import the Pandas library
 import pandas as pd
+import numpy as np
+from sklearn import tree
 def log(msg):
 	print("****************************************************************************\n* "+msg)
 
@@ -11,11 +13,12 @@ test_url = "http://s3.amazonaws.com/assets.datacamp.com/course/Kaggle/test.csv"
 test = pd.read_csv(test_url)
 
 # Convert the male and female groups to integer form
+# male = 0
+# female = 1
 train["Sex"][train["Sex"] == "male"] = 0
 train["Sex"][train["Sex"] == "female"] = 1
-# Impute the Embarked variable
+# Impute the Embarked variable with the most common value (S)
 train["Embarked"] = train["Embarked"].fillna("S")
-# print train["Embarked"].describe()
 
 # # Convert the Embarked classes to integer form
 # S = 0
@@ -25,5 +28,24 @@ train["Embarked"][train["Embarked"] == "S"] = 0
 train["Embarked"][train["Embarked"] == "C"] = 1
 train["Embarked"][train["Embarked"] == "Q"] = 2
 
-# #Print the Sex and Embarked columns
-print(train[['Sex', 'Embarked']])
+# Set the median value for Age if none is provided
+train["Age"] = train["Age"].fillna(train["Age"].median())
+
+
+
+log("Now let's do some ML...")
+
+# Print the train data to see the available features
+
+# Create the target and features numpy arrays: target, features_one
+target = train["Survived"].values
+features_one = train[["Pclass", "Sex", "Age", "Fare"]].values
+
+# Fit your first decision tree: my_tree_one
+my_tree_one = tree.DecisionTreeClassifier()
+my_tree_one = my_tree_one.fit(features_one, target)
+
+log("Show the importance of the included features - PClass, Sex, Age, Fare")
+print(my_tree_one.feature_importances_)
+log("Show the score of the included features - PClass, Sex, Age, Fare")
+print(my_tree_one.score(features_one, target))
