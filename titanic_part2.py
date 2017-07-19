449c20d52d2f428330d1ed94ba59175da6ad2163
David Campelo <david.campelo@gmail.com>
2017-07-19 23:03:38 +0100

    Overfitting and how to control it When you created your first decision tree the default arguments for max_depth and min_samples_split were set to None. This means that no limit on the depth of your tree was set. That's a good thing right? Not so fast. We are likely overfitting. This means that while your model describes the training data extremely well, it doesn't generalize to new data, which is frankly the point of prediction. Just look at the Kaggle submission results for the simple model based on Gender and the complex decision tree. Which one does better?
    Maybe we can improve the overfit model by making a less complex model? In DecisionTreeRegressor, the depth of our model is defined by two parameters: - the max_depth parameter determines when the splitting up of the decision tree stops. - the min_samples_split parameter monitors the amount of observations in a bucket. If a certain threshold is not reached (e.g minimum 10 passengers) no further splitting can be done.
    
    By limiting the complexity of your decision tree you will increase its generality and thus its usefulness for prediction!
    
    - Include the Siblings/Spouses Aboard, Parents/Children Aboard, and Embarked features in a new set of features.
    - Fit your second tree my_tree_two with the new features, and control for the model compelexity by toggling the max_depth and min_samples_split arguments.

--- titanic_part2.py
+++ titanic_part2.py
@@ -12,13 +12,17 @@ train = pd.read_csv(train_url)
 test_url = "http://s3.amazonaws.com/assets.datacamp.com/course/Kaggle/test.csv"
 test = pd.read_csv(test_url)
 
+print test
 # Convert the male and female groups to integer form
 # male = 0
 # female = 1
 train["Sex"][train["Sex"] == "male"] = 0
 train["Sex"][train["Sex"] == "female"] = 1
+test["Sex"][test["Sex"] == "male"] = 0
+test["Sex"][test["Sex"] == "female"] = 1
 # Impute the Embarked variable with the most common value (S)
 train["Embarked"] = train["Embarked"].fillna("S")
+test["Embarked"] = test["Embarked"].fillna("S")
 
 # # Convert the Embarked classes to integer form
 # S = 0
@@ -27,9 +31,13 @@ train["Embarked"] = train["Embarked"].fillna("S")
 train["Embarked"][train["Embarked"] == "S"] = 0
 train["Embarked"][train["Embarked"] == "C"] = 1
 train["Embarked"][train["Embarked"] == "Q"] = 2
+test["Embarked"][test["Embarked"] == "S"] = 0
+test["Embarked"][test["Embarked"] == "C"] = 1
+test["Embarked"][test["Embarked"] == "Q"] = 2
 
 # Set the median value for Age if none is provided
 train["Age"] = train["Age"].fillna(train["Age"].median())
+test["Age"] = test["Age"].fillna(test["Age"].median())
 
 
 
@@ -49,3 +57,38 @@ log("Show the importance of the included features - PClass, Sex, Age, Fare")
 print(my_tree_one.feature_importances_)
 log("Show the score of the included features - PClass, Sex, Age, Fare")
 print(my_tree_one.score(features_one, target))
+
+# Impute the missing value with the median
+test.Fare[152] = test["Fare"].median()
+
+# Extract the features from the test set: Pclass, Sex, Age, and Fare.
+test_features = test[["Pclass", "Sex", "Age", "Fare"]].values
+
+# Make your prediction using the test set
+my_prediction = my_tree_one.predict(test_features)
+
+# Create a data frame with two columns: PassengerId & Survived. Survived contains your predictions
+PassengerId =np.array(test["PassengerId"]).astype(int)
+my_solution = pd.DataFrame(my_prediction, PassengerId, columns = ["Survived"])
+
+log(" Writing solution to a csv file with the name my_solution.csv")
+my_solution.to_csv("my_solution_one.csv", index_label = ["PassengerId"])
+
+
+# Create a new array with the added features: features_two
+features_two = train[["Pclass","Age","Sex","Fare", "SibSp", "Parch", "Embarked"]].values
+
+#Control overfitting by setting "max_depth" to 10 and "min_samples_split" to 5 : my_tree_two
+max_depth = 10
+min_samples_split = 5
+my_tree_two = tree.DecisionTreeClassifier(max_depth = 10, min_samples_split = 5, random_state = 1)
+my_tree_two = my_tree_two.fit(features_two, target)
+
+# #Print the score of the new decison tree
+log("Show the importance of the included features - PClass, Sex, Age, Fare, SibSp, Parch, Embarked")
+print(my_tree_two.feature_importances_)
+log("Show the score of the included features - PClass, Sex, Age, Fare, SibSp, Parch, Embarked")
+print(my_tree_two.score(features_two, target))
+
+
+
