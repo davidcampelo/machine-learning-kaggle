becdb0e45dad28c5a03671f70c28c7a7e6c4e607
David Campelo <david.campelo@gmail.com>
2017-07-19 22:15:13 +0100

    - Assign the integer 1 to all females - Impute missing values in Embarked with class S. Use .fillna() method. - Replace each class of Embarked with a uniques integer. 0 for S, 1 for C, and 2 for Q. - Print the Sex and Embarked columns

--- titanic_part2.py
+++ titanic_part2.py
@@ -7,6 +7,23 @@ def log(msg):
 # Load the train and test datasets to create two DataFrames
 train_url = "http://s3.amazonaws.com/assets.datacamp.com/course/Kaggle/train.csv"
 train = pd.read_csv(train_url)
-
 test_url = "http://s3.amazonaws.com/assets.datacamp.com/course/Kaggle/test.csv"
 test = pd.read_csv(test_url)
+
+# Convert the male and female groups to integer form
+train["Sex"][train["Sex"] == "male"] = 0
+train["Sex"][train["Sex"] == "female"] = 1
+# Impute the Embarked variable
+train["Embarked"] = train["Embarked"].fillna("S")
+# print train["Embarked"].describe()
+
+# # Convert the Embarked classes to integer form
+# S = 0
+# C = 1
+# Q = 2
+train["Embarked"][train["Embarked"] == "S"] = 0
+train["Embarked"][train["Embarked"] == "C"] = 1
+train["Embarked"][train["Embarked"] == "Q"] = 2
+
+# #Print the Sex and Embarked columns
+print(train[['Sex', 'Embarked']])
