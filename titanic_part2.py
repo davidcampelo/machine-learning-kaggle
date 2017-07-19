957039f92c5af9496fb25c6508940a04595de088
David Campelo <david.campelo@gmail.com>
2017-07-19 21:56:30 +0100

    Creating the basics for the second part of the course - Prediction with decision trees

---
+++ titanic_part2.py
@@ -0,0 +1,12 @@
+# Import the Pandas library
+import pandas as pd
+def log(msg):
+	print("****************************************************************************\n* "+msg)
+
+
+# Load the train and test datasets to create two DataFrames
+train_url = "http://s3.amazonaws.com/assets.datacamp.com/course/Kaggle/train.csv"
+train = pd.read_csv(train_url)
+
+test_url = "http://s3.amazonaws.com/assets.datacamp.com/course/Kaggle/test.csv"
+test = pd.read_csv(test_url)
